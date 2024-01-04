6db6389a33adf3ebc0899324eb2ff79028f0e2a1
alisha raghav <alisharaghav09@gmail.com>
2024-01-04 18:56:44 +0530

    added views

--- expense/home/models.py
+++ expense/home/models.py
@@ -16,6 +16,7 @@ class Profile(models.Model):
 class Expense(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     name=models.CharField(max_length=100)
+    amount=models.FloatField()
     expense_type=models.CharField(max_length=100, choices=TYPE)
 
     def __str__(self):
